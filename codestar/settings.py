2e3e010896d78fd0769888c3382f862e6274ae14
Keevin Aroco <gavikde2022@gmail.com>
2025-02-16 11:13:26 +0100

    Add Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -92,6 +92,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.codeinstitute-ide.net/",
+    "http://*herokuapp.com/"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
